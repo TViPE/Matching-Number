1d4b5485c742ffbded5ae3de4a8d965b6ebdf845
TRI VI <trivi2710@gmail.com>
2017-06-08 19:42:45 -0400

    compareArrElem function
    Feat: compareArrElem()
    
    this function will compare 2 buttons after clicked. If the values of
    two buttons are the same, the text of button will remain showing. If
    the values are not same, keep hiding the text.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var arr =[0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7];
-var compareArr = []
+var compareArr = [];
 var count = 0;
 var oldVal;
 var isMatch = false;
+var btnID = -1;
 
 
 //shuffle elements randomly
@@ -22,28 +23,44 @@ function shuffle(arr){
 		arr[currentIndex] = arr[randomIndex];
 		arr[randomIndex] = temporaryValue;
 	}
-
 	return arr;
 }
 
 function toggleText(){
 	$('.boardBtn').click(function(){
+
+		var anObj = {};
+			anObj.index = $(this).attr('id');
+		// Change the color of text to black
 		$(this).css("color", "black");
+
 		$(this).text(function(){
+			// Get the id of button from the array
 			var idVal = $(this).attr('id');
+
+			// Get the value of button
 			var preVal = $(this).text();
+
+			// Compare with default text to show/hide text
 			if (preVal == "#"){
 				$(this).text(arr[idVal]);
 				oldVal = $(this).text();
-				
-				console.log(oldVal);
 			} else {
 				$(this).text("#");
 			}
 		});
-		compareArr.push(oldVal);
+		
+		anObj.val = oldVal;
+		compareArr.push(anObj);
 		count++;
-		compareArrElem(compareArr);
+		console.log(compareArr);
+		if(count == 2){
+			console.log(compareArr);
+			compareArrElem();
+			count = 0;
+		}
+
+			
 	});
 }
 
@@ -51,26 +68,35 @@ function hideText(){
 	$('.boardBtn').css("color", "transparent");
 }
 
-function compareArrElem(array){
-	if(count == 2) {
-		if(array[0] == array[1]){
-			isMatch = true;
-			window.alert("Win");
-			count = 0;
-		} else {
-			isMatch = false;
-		}
+function compareArrElem(){
+	if ((compareArr[0].val == compareArr[1].val) && (compareArr[0].index !== compareArr[1].index)){
+		isMatch = true;
+		console.log("Win");
+		compareArr = [];
+	} else if (compareArr[0].val !== compareArr[1].val){
+		console.log(compareArr);
+		isMatch = false;
+
+		setTimeout(function(){
+			$('#' + compareArr[0].index).text("#");
+			$('#' + compareArr[1].index).text("#");
+			compareArr = [];
+			//$('#' + array[1].index).text("#");
+		}, 1000);
 	}
 }
 
 
+
+
 // Main function
 $(function(){
-	var tempArr = shuffle(arr);
-	console.log(tempArr);
+	shuffle(arr);
+	console.log(arr);
 	//assignText(tempArr);
 	//hideText();
 	toggleText();
+
 
 
 })
